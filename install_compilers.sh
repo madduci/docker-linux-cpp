95405918534154dc1234379a013cd972ba11275a
Ayaz Salikhov <asalikhov@aimtech.com>
2020-04-27 19:48:13 +0200

    Fix bash codestyle

--- install_compilers.sh
+++ install_compilers.sh
@@ -3,21 +3,21 @@
 readonly DEBIAN_PACKAGES="${1}"
 readonly CLANG_PACKAGES="${2}"
 
-DEBIAN_FRONTEND=noninteractive 
-apt-get update 
+DEBIAN_FRONTEND=noninteractive
+apt-get update
 
 echo "Installing ${DEBIAN_PACKAGES}"
 apt-get install -y ${DEBIAN_PACKAGES}
 
-wget -O /tmp/llvm.sh https://apt.llvm.org/llvm.sh 
-chmod +x /tmp/llvm.sh 
+wget -O /tmp/llvm.sh https://apt.llvm.org/llvm.sh
+chmod +x /tmp/llvm.sh
 
 echo "Installing ${CLANG_PACKAGES}"
 for version in ${CLANG_PACKAGES}
-do 
-   /tmp/llvm.sh $version
-done 
+do
+    /tmp/llvm.sh $version
+done
 
-apt-get autoremove --purge -y 
-apt-get autoclean -y 
-rm -rf /var/cache/apt/* /tmp/* 
+apt-get autoremove --purge -y
+apt-get autoclean -y
+rm -rf /var/cache/apt/* /tmp/*
